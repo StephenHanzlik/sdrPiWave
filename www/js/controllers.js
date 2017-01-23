f0bff60ac10fff1824b0dc4c0c4d71bbd26deea4
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-01-22 19:16:59 -0700

    commit to pull keegans changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,30 +14,37 @@ angular.module('app.controllers', [])
   .controller('homeCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function($scope, $stateParams, $http) {
-      alert("hello from controller");
-      const vm = this;
 
-      vm.items = ["dinky", "jinky", "slinky"];
-      console.log(vm.items);
+    // function($scope, $stateParams, $http) {
+    //   alert("hello from controller");
+    //   const vm = this;
+    //
+    //   vm.items = ["dinky", "jinky", "slinky"];
+    //   console.log(vm.items);
 
-      // $http.get("http://eggnogg:8000/uploads")
-      //   .success(function(response) {
-      //     alert("We have hit the route");
-      //     vm.posts = response.data;
-      //     let name = '';
-      //     let username = '';
-      //
-      //     for (let bit of response) {
-      //       name = bit.name;
-      //       username = bit.username;
-      //     }
-      //     alert("name " + name + " *** " + "username " + username);
-      //   })
-      //   .error(function(data) {
-      //     alert("error");
-      //   })
+    function MyCtrl($scope, $ionicListDelegate, $stateParams, $http) {
+      $scope.items = ["dinky", "jinky", "slinky"];
+      $scope.showDeleteButtons = function() {
+        $ionicListDelegate.showDelete(true);
+      };
     }
+
+    // $http.get("http://eggnogg:8000/uploads")
+    //   .success(function(response) {
+    //     alert("We have hit the route");
+    //     vm.posts = response.data;
+    //     let name = '';
+    //     let username = '';
+    //
+    //     for (let bit of response) {
+    //       name = bit.name;
+    //       username = bit.username;
+    //     }
+    //     alert("name " + name + " *** " + "username " + username);
+    //   })
+    //   .error(function(data) {
+    //     alert("error");
+    //   })
   ])
 
   .controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
