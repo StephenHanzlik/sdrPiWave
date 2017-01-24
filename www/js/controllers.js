9c52aa18769c88844ae1cdf5b8622b209b9494a0
Kyle Gray <th33nd3r@gmail.com>
2017-01-24 10:50:50 -0700

    wrote angular html for search feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,11 +26,20 @@ angular.module('app.controllers', ['ionic'])
       }
     }
   ])
-  .controller('searchCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('searchCtrl', ['$scope', '$stateParams', 'filesService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams) {
+    function ($scope, $stateParams, filesService) {
+      const vm = this;
+      console.log(filesService.files);
+      vm.search = search;
 
+      function search(){
+        filesService.getFiles()
+          .then((results) => {
+            vm.data = results.data;
+          });
+      }
     }
   ])
 
