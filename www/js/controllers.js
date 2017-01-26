2b54ef1e632fe9f8d1a22fd71c378710b270f9ec
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-01-25 22:07:39 -0700

    :sparkles: successful use of angular timeout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -98,7 +98,7 @@ angular.module('app.controllers', ['ionic'])
     }
   ])
 
-  .controller('landingCtrl', ['$scope', '$stateParams', '$ionicModal', '$http', '$timeout' // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('landingCtrl', ['$scope', '$stateParams', '$ionicModal', '$http', '$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
 
