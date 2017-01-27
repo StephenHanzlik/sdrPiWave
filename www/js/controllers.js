0fef31f7dfb4f368f67f14f2eea09dc60ec83eed
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-01-27 11:43:44 -0700

    have logic working in controller to match avatar paths

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,10 +10,10 @@ angular.module('app.controllers', ['ionic'])
     }
   ])
 
-  .controller('homeCtrl', ['$scope', '$stateParams', 'filesService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('homeCtrl', ['$scope', '$stateParams', 'filesService', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
     // You can include any angular dependencies as parameters for this function
     // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function($scope, $stateParams, filesService) {
+    function($scope, $stateParams, filesService, $http) {
       const vm = this;
       vm.$onInit = onInit;
       vm.playback = playback;
@@ -23,6 +23,32 @@ angular.module('app.controllers', ['ionic'])
         filesService.getFiles()
           .then((res) => {
             vm.data = res.data;
+          });
+        $http.get("http://eggnogg:8000/users")
+          .success(function(userProfile) {
+            vm.userdata = userProfile;
+            // console.log('vm.data[1]' + vm.data[1].username);
+            // console.log('vm.userdata' + vm.userdata[1].avatar_path);
+            // console.log('vm.userdata[1]' + vm.userdata[1].username);
+            // console.log('vm.userdata[5]' + vm.userdata[5].username);
+            // var userdataArray = vm.userdata;
+            // console.log("vm.data in get succes" + vm.data);
+            for (var t = 0; t < vm.userdata.length; t++) {
+              // console.log("vm.userdata[i]  " + vm.userdata[i].username);
+              for (var i = 0; i < vm.data.length; i++) {
+                // console.log("vm.data[i]" + vm.data[i].username);
+                // console.log("vm.userdata[i]  " + vm.userdata[t].username);
+                if (vm.data[i].username === vm.userdata[t].username) {
+                  vm.data[i].avatar_path = vm.userdata[t].avatar_path;
+                  console.log("samsies in data? " + vm.data[i].avatar_path);
+                  console.log("samsies in userdata? " + vm.userdata[t].avatar_path);
+                }
+              }
+              // console.log("vm.userdata[i]" + vm.userdata[i]);
+            }
+          })
+          .error(function(data) {
+            alert(`error: ${data}`);
           });
       }
 
